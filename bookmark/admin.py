04300b39e52abc2d5198bdd4e92260e3614900f6
haxlys <haxlys@gmail.com>
2017-01-12 00:41:49 +0900

    오타 수정..

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from bookmark.models import bookmark
+from bookmark.models import Bookmark
 
 # Register your models here.
 
--- bookmark/models.py
+++ bookmark/models.py
@@ -1,5 +1,3 @@
-from django.db import models
-
 from __future__ import unicode_literals # python 2.x 지원용
 
 from django.db import models
@@ -8,7 +6,7 @@ from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
 
 @python_2_unicode_compatible    # Python 2.x 지원용
-class Bookmark(model.Model):
+class Bookmark(models.Model):
     title = models.CharField(max_length=100, blank=True, null=True) # 공백과 null 허용
     url = models.URLField('url', unique=True) # 매개변수 url 문자열은 url 컬럼에 대한 레이블 문구. 별칭.
 
