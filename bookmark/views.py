32879c99cf6646991f6bcc43f4b39e07e2c37861
haxlys <haxlys@gmail.com>
2017-01-12 01:42:49 +0900

    즐겨찾기 기능 완료

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from bookmark.models import Bookmark
 
 # Create your views here.
+
+#--- ListView
+class BookmarkLV(ListView):
+    model = Bookmark
+# ListView 에서 자동으로 설정해주는 2가지
+# 1. 컨텍스트 변수로 object_list 사용
+# 2. 템플릿으로 모델명소문자_list.html 사용
+
+#--- DetailView
+class BookmarkDV(DetailView):
+    model = Bookmark
+# DetailView 에서 자동으로 설정해주는 2가지
+# 1. 컨텍스트 변수로 object 사용
+# 2. 템플릿으로 모델명소문자_detail.html 사용
+
+
+# 리턴해주는 템플릿이과 컨텍스트 변수가 없지만 자동으로 설정해줌
+# 디테일 같은 경우에는 url로 부터 넘어오는 매개변수를 통해서 가져온다고 함. urls.py에 정의한 매개변수명(pk)을 변경해도 인식할까?
+# pk는 장고가 인식하는 고유의 변수명 같다. 다른 변수명을 명시할 경우 에러를 발생시킨다.
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,26 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from bookmark.views import BookmarkLV, BookmarkDV
+#from bookmark.views import *   # * 사용 가능
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', include(admin.site.urls)),
+    # admin.site.urls 는 예외적으로 include() 함수를 사용하지 않아도 됨.
+
+    # Class-based views for Bookmark app
+    url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
+    url(r'^bookmark/(?P<pk>\d+)$', BookmarkDV.as_view(), name='detail'),
+
+    # url(regex, view, kwargs=None, name=None, prefix='')
+    # 앞의 2개는 필수 인자, 뒤의 3개는 선택 인자.
+    # patterns() 함수는 1.8 이후로 사용되지 않음.
 ]
+
+# 간단한 로직은 아래와 같이 views.py 에서 처리하지 않고 바로 urls.py에서 처리 할 수도 있다.
+# from django.views.generic import ListView, DetailView
+# from bookmark.models import Bookmark
+
+    # url(r'^bookmark/$', ListView.as_view(model=Bookmark), name='index'),
+    # url(r'^bookmark/(?P<pk>\d+)$', DetailView.as_view(model=Bookmark), name='detail'),
