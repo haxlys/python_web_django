50ea87d1b000cf3aa3a38787be9e7e0fa15c0b5e
haxlys <haxlys@gmail.com>
2017-01-12 00:29:07 +0900

    bookmark 모델을 추가 admin에도 bookmark 모델 관련된 사항 추가

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from bookmark.models import bookmark
 
 # Register your models here.
+
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url')
+
+# Bookmark 모델을 BookmarkAdmin 클래스에 정의한데로 Admin화면에서 보여진다.
+admin.site.register(Bookmark, BookmarkAdmin)
--- bookmark/models.py
+++ bookmark/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from __future__ import unicode_literals # python 2.x 지원용
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
 # Create your models here.
+
+@python_2_unicode_compatible    # Python 2.x 지원용
+class Bookmark(model.Model):
+    title = models.CharField(max_length=100, blank=True, null=True) # 공백과 null 허용
+    url = models.URLField('url', unique=True) # 매개변수 url 문자열은 url 컬럼에 대한 레이블 문구. 별칭.
+
+    def __str__(self):
+        return self.title
