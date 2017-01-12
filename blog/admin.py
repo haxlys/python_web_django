d4df6400c0ecb5a5567dd0431b9d5c3947d2f1e1
haxlys <haxlys@gmail.com>
2017-01-12 10:11:08 +0900

    Post 모델 추가 및 관리자 설정 추가 title, slug, description, content, create_date, modify_date

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+# 관리자 화면에서 보여질 모습 정의
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'modify_date')  # 리스트화면에서 보여줄 컬럼 정의
+    list_filter = ('modify_date',) # 필터사이드바 사용할 컬럼 정의
+    search_fields = ('title', 'content') # 검색박스 사용 및 검색 시 적용할 컬럼 정의
+    prepopulated_fields = {'slug': ('title',)} # slug필드는 title을 이용해 미리 채워지도록 설정
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
+from __future__ import unicode_literals # python 2.x 지원용
+from django.utils.encoding import python_2_unicode_compatible
+
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
+
+@python_2_unicode_compatible    # Python 2.x 지원용
+class Post(models.Model):
+    title = models.CharField('TITLE', max_length=50) # 공백과 null 허용
+    slug = models.SlugField('SLUG', unique=True, allow_unicode=True, help_text='one word for title alias.') # 매개변수 url 문자열은 url 컬럼에 대한 레이블 문구. 별칭.
+    description = models.CharField('DESCRIPTION', max_length=100, blank=True, help_text='simple description text.')
+    content = models.TextField('CONTENT')
+    create_date = models.DateTimeField('Create Date', auto_now_add=True)
+    modify_date = models.DateTimeField('Modify Date', auto_now=True)
+
+    class Meta:
+        verbose_name = 'post' # 테이블의 별칭 (단수)
+        verbose_name_plural = 'posts' #테이블의 별칭 (복수)
+        db_table = 'my_post' # db에 저장되는 테이블 명(미지정시 앱이름_모델클래스명 blog_post 가 됨)
+        ordering = ('-modify_date',) # order by 기본 형식('-'가 붙으면 내림차순, !!! ordering은 tuple이나 list형태여야 하므로 마지막 , 을 붙여줘야한다.)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self): # 지정된 하나의 데이터를 args로 부터 가져옵니다.
+        return reverse('blog:post_detail', args=(self.slug,))
+
+    def get_previous_post(self):
+        return self.get_previous_by_modify_date()
+
+    def get_next_post(self):
+        return self.get_next_by_modify_date()
